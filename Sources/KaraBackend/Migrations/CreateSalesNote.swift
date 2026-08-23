2fd361aa9e5393d8fd0c5b82cbc4796168543d1c
Sherin Olivia <sochronicle@gmail.com>
2026-08-23 13:18:22 +0700

    Feat: Created the migration files for each table

---
+++ Sources/KaraBackend/Migrations/CreateSalesNote.swift
@@ -0,0 +1,35 @@
+import Fluent
+
+struct CreateSalesNote: AsyncMigration {
+    func prepare(on database: any Database) async throws {
+        let status = try await database.enum("status")
+            .case("paid")
+            .case("dp_paid")
+            .case("not_paid")
+            .create()
+        
+        try await database.schema("sales_notes")
+            .id()
+            .field("shop_id", .uuid, .required, .references("shops", "id"))
+            .field("identifier", .string, .required)
+            .field("customer_name", .string, .required)
+            .field("customer_phone", .string)
+            .field("total_amount", .int, .required)
+            .field("paid_amount", .int, .required)
+            .field("status", status, .required)
+            .field("note_file_link", .string)
+            .field("due_at", .datetime, .required)
+            .field("sold_at", .datetime, .required)
+            .field("created_at", .datetime, .required)
+            .field("updated_at", .datetime, .required)
+            .field("created_by", .string, .required)
+            .field("updated_by", .string, .required)
+            .field("is_deleted", .bool)
+            .create()
+    }
+    
+    func revert(on database: any Database) async throws {
+        try await database.schema("sales_notes").delete()
+        try await database.enum("status").delete()
+    }
+}
---
+++ Sources/KaraBackend/Migrations/CreateSalesNoteItem.swift
@@ -0,0 +1,18 @@
+import Fluent
+
+struct CreateSalesNoteItem: AsyncMigration {
+    func prepare(on database: any Database) async throws {
+        try await database.schema("sales_note_items")
+            .id()
+            .field("sales_note_id", .uuid, .required, .references("sales_notes", "id"))
+            .field("name", .string)
+            .field("quantity", .int, .required)
+            .field("unit_price", .int, .required)
+            .field("subtotal", .int, .required)
+            .create()
+    }
+    
+    func revert(on database: any Database) async throws {
+        try await database.schema("sales_note_items").delete()
+    }
+}

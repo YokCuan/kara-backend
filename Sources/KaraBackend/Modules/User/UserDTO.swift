64b150df5bbe4ab353e9533450579380a0ae19fb
Sherin Olivia <sochronicle@gmail.com>
2026-08-24 12:56:39 +0700

    fix: add missing '}' in UserResponseDTO

--- Sources/KaraBackend/Modules/User/UserDTO.swift
+++ Sources/KaraBackend/Modules/User/UserDTO.swift
@@ -10,17 +10,18 @@ struct CreateUserDTO: Content {
     let name: String
     let phone: String
     let password: String?
+}
 
 struct UserResponseDTO: Content {
     let id: UUID
     let name: String
     let phone: String
-
+    
     init(user: User) throws {
         guard let id = user.id else {
             throw Abort(.internalServerError, reason: "User ID is missing")
         }
-
+        
         self.id = id
         self.name = user.name
         self.phone = user.phone

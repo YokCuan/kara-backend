26e8133a5c570cc5ccb8b5a46e2a6acf9cc2dcbb
Sherin Olivia <sochronicle@gmail.com>
2026-08-24 12:16:18 +0700

    feat: make the register and login functions

---
+++ Sources/KaraBackend/Modules/Authentication/AuthenticationController.swift
@@ -0,0 +1,24 @@
+// Modules/Authentication/AuthController.swift
+import Vapor
+import Fluent
+
+struct AuthenticationController: RouteCollection {
+    let authenticationService: any AuthenticationServiceProtocol
+
+    func boot(routes: RoutesBuilder) throws {
+        let auth = routes.grouped("auth")
+
+        auth.post("register", use: register)
+        auth.post("login", use: login)
+    }
+
+    func register(req: Request) async throws -> RegisterResponseDTO {
+        let dto = try req.content.decode(RegisterDTO.self)
+        return try await authenticationService.register(dto, on: req.db)
+    }
+
+    func login(req: Request) async throws -> UserResponseDTO {
+        let dto = try req.content.decode(LoginDTO.self)
+        return try await authenticationService.login(dto, on: req.db)
+    }
+}

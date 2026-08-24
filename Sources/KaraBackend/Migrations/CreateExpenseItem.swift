9daf03bd42bcef1701a51a0d429d5e3559c9a28e
Sherin Olivia <sochronicle@gmail.com>
2026-08-24 23:17:16 +0700

    feat: add onDelete cascade on expense items by expense Id

--- Sources/KaraBackend/Migrations/CreateExpenseItem.swift
+++ Sources/KaraBackend/Migrations/CreateExpenseItem.swift
@@ -4,6 +4,7 @@ struct CreateExpenseItem: AsyncMigration {
     func prepare(on database: any Database) async throws {
         try await database.schema("expense_items")
             .id()
+            .field("expense_id", .uuid, .required, .references("expenses", "id",onDelete: .cascade))
             .field("name", .string, .required)
             .create()
     }

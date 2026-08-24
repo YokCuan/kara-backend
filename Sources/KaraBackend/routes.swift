c1bdabb33a1f721b0c34633bd9a5a62ecb0cc0de
Sherin Olivia <sochronicle@gmail.com>
2026-08-24 12:16:31 +0700

    feat: register the authentication route
    (cherry picked from commit 7824be603bc99285bf7d7d19db63efacd8117c79)

--- Sources/KaraBackend/routes.swift
+++ Sources/KaraBackend/routes.swift
@@ -4,8 +4,23 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
-
+    
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    let userRepository = UserRepository()
+    let shopRepository = ShopRepository()
+    let userService = UserService(userRepository: userRepository)
+    let shopService = ShopService(shopRepository: shopRepository)
+    let authenticationService = AuthenticationService(userService: userService, shopService: shopService)
+    
+    let userController = UserController(userService: userService)
+    try app.register(collection: userController)
+    
+    let shopController = ShopController(shopService: shopService)
+    try app.register(collection: shopController)
+    
+    let authenticationController = AuthenticationController(authenticationService: authenticationService)
+    try app.register(collection: authenticationController)
 }

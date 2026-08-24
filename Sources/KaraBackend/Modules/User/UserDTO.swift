cb05385798b621de5acb585160123df8f15bc2b2
Sherin Olivia <sochronicle@gmail.com>
2026-08-24 12:15:07 +0700

    fix: make the password nullable in the DTO layer too
    (cherry picked from commit 23465f40a3b479898b558f226e52f7b457a9bcc9)

---
+++ Sources/KaraBackend/Modules/User/UserDTO.swift
@@ -0,0 +1,33 @@
+import Vapor
+
+struct APIResponseDTO<T: Content>: Content {
+    let status: String
+    let message: String
+    let data: T?
+}
+
+struct CreateUserDTO: Content {
+    let name: String
+    let phone: String
+    let password: String?
+}
+
+struct UserResponseDTO: Content {
+    let id: UUID
+    let name: String
+    let phone: String
+
+    init(user: User) throws {
+        guard let id = user.id else {
+            throw Abort(.internalServerError, reason: "User ID is missing")
+        }
+
+        self.id = id
+        self.name = user.name
+        self.phone = user.phone
+    }
+}
+
+struct FindByPhoneDTO: Content {
+    let phone: String
+}

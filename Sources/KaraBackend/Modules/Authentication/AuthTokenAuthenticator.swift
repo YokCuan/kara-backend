3c967cd9a8868bd97b0240bbee162eae51d06171
Sherin Olivia <sochronicle@gmail.com>
2026-09-07 11:29:37 +0700

    Feat: created helper function to authenticate the auth token

--- Sources/KaraBackend/Modules/Authentication/AuthTokenAuthenticator.swift
+++ Sources/KaraBackend/Modules/Authentication/AuthTokenAuthenticator.swift
@@ -1,7 +1,30 @@
-//
-//  AuthTokenAuthentication.swift
-//  KaraBackend
-//
-//  Created by Sherin Olivia on 07/09/26.
-//
+import Vapor
+import Fluent
 
+extension User: Authenticatable {}
+
+struct AuthTokenAuthenticator: AsyncBearerAuthenticator {
+    let authTokenService: any AuthTokenServiceProtocol
+    let userRepository: any UserRepositoryProtocol
+
+    func authenticate(
+        bearer: BearerAuthorization,
+        for request: Request
+    ) async throws {
+        guard let token = try await authTokenService.findActive(
+            rawToken: bearer.token,
+            on: request.db
+        ) else {
+            return
+        }
+
+        guard let user = try await userRepository.findById(
+            token.userId,
+            on: request.db
+        ) else {
+            return
+        }
+
+        request.auth.login(user)
+    }
+}
